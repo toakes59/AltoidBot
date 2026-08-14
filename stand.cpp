9ccb1c5a2782e1f15cefd49b59eff7d9ad921ca0
Tom <toakes01@manhattan.edu>
2026-08-13 21:01:01 -0400

    Fixed sit and added laydown

--- stand.cpp
+++ stand.cpp
@@ -10,6 +10,17 @@ void stand()
 
 void sit()
 {
+  // Front legs fold back a little, rear legs fold further, lowering the rear
+  // of the body while the front stays more upright.
+  frontLeft.write(clampAngle(105 + frontLeftTrim));
+  frontRight.write(clampAngle(75 + frontRightTrim));
+
+  rearLeft.write(clampAngle(30 + rearLeftTrim));
+  rearRight.write(clampAngle(150 + rearRightTrim));
+}
+
+void layDown()
+{
   // Fold the legs further than a walking stride so the body lowers to the ground.
   frontLeft.write(clampAngle(180 + frontLeftTrim));
   rearRight.write(clampAngle(180 + rearRightTrim));
